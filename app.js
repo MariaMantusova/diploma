24c6451e1f06a5f6e09a17e09b77ccde55de9a5b
MariaMantusova <maria.mantusova@mail.ru>
2023-03-01 12:53:46 +0300

    feat: add cors

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ const limiter = rateLimit({
   max: 100,
 });
 
+const allowedCors = [
+  'localhost:3000',
+];
+
 const userRouter = require('./routes/users');
 const movieRouter = require('./routes/movies');
 
@@ -26,6 +30,25 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 app.use(helmet());
 app.use(limiter);
 app.use(bodyParser.json());
+app.use('/', (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+});
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
